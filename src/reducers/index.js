8b7ed2324d7b1aaaa8175c01740a43c0d5ebde40
Nathan Zukoff <nzukoff@gmail.com>
2018-10-25 09:57:25 -0700

    Change host to heroku

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,8 +8,8 @@ const initialState = {
     button_styles: [],
     percentage: 0, 
     choices: [],
-    host: 'http://127.0.0.1:5000/'
-    // host: 'https://pixel-game-api.herokuapp.com/'
+    // host: 'http://127.0.0.1:5000/'
+    host: 'https://pixel-game-api.herokuapp.com/'
 }
   
   const rootReducer = (state = initialState, action) => {
